22189da77154288d98544e817ced7b0c2370cb8c
salvador <josesalvadorpp@hotmail.com>
2021-05-07 18:58:25 -0400

    added test for create currency format endpoint

--- src/tests/currencyFormat.js
+++ src/tests/currencyFormat.js
@@ -5,13 +5,15 @@ const chai = require('chai');
 const expect = chai.expect;
 const chaihttp = require('chai-http');
 const { app } = require('../../index');
-
+const sinon = require('sinon');
 chai.use(chaihttp);
 
 //Importing our model for our unit testing.
 var CurrencyFormat = require('../models/currencyFormats');
 
 describe('currencyFormat_controller', function() {
+	sinon.stub(console, 'log'); // disable console.log on tests
+
 	beforeEach((done) => {
 		//Before each test we empty the database
 		CurrencyFormat.deleteMany({}, (err) => {
@@ -92,6 +94,79 @@ describe('currencyFormat_controller', function() {
 
 			expect(res.body.data.currencyFormats).to.be.an('array');
 			expect(res.body.data.currencyFormats.length).to.equal(1);
+		});
+	});
+
+	// endpoint to create a new currency format
+	describe('createFormat', function() {
+		it('should create a new currency format', async function() {
+			let newCurrencyFormat = {
+				currencyAfterPrice: false,
+				showCents: true,
+				thousandDelimeter: 'comma',
+				currencyDisplay: 'symbol',
+				marketCountry: 'Spain',
+				currency: 'EUR'
+			};
+
+			let res = await chai.request(app).post('/currencyFormat').send(newCurrencyFormat);
+
+			expect(res).to.have.status(200);
+			expect(res).to.be.json;
+			expect(res.body).to.be.an('object');
+
+			expect(res.body).to.have.property('type').eql('Success');
+			expect(res.body).to.have.property('message').eql('entry created successfully in database');
+			expect(res.body).to.have.property('data');
+			expect(res.body.data).to.have.property('currencyFormat');
+
+			expect(res.body.data.currencyFormat).to.be.an('object');
+		});
+
+		it('should return error if the market country is not available country (i.e Narnia)', async function() {
+			let newCurrencyFormat = {
+				currencyAfterPrice: false,
+				showCents: true,
+				thousandDelimeter: 'comma',
+				currencyDisplay: 'symbol',
+				marketCountry: 'Narnia',
+				currency: 'EUR'
+			};
+
+			let res = await chai.request(app).post('/currencyFormat').send(newCurrencyFormat);
+
+			expect(res).to.have.status(200);
+			expect(res).to.be.json;
+			expect(res.body).to.be.an('object');
+			expect(res.body).to.have.property('type').eql('Error');
+		});
+
+		it('should return error if try to create an entry with a country and currency that already exist in database', async function() {
+			let AlreadyExists = new CurrencyFormat({
+				currencyAfterPrice: false,
+				showCents: true,
+				thousandDelimeter: 'comma',
+				currencyDisplay: 'symbol',
+				marketCountry: 'Spain',
+				currency: 'EUR'
+			});
+			await AlreadyExists.save();
+
+			let newCurrencyFormat = {
+				currencyAfterPrice: false,
+				showCents: true,
+				thousandDelimeter: 'comma',
+				currencyDisplay: 'symbol',
+				marketCountry: 'Spain',
+				currency: 'EUR'
+			};
+
+			let res = await chai.request(app).post('/currencyFormat').send(newCurrencyFormat);
+
+			expect(res).to.have.status(200);
+			expect(res).to.be.json;
+			expect(res.body).to.be.an('object');
+			expect(res.body).to.have.property('type').eql('Error');
 		});
 	});
 });
